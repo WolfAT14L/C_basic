f9c58e1477b50defe9aa667499cc9d24cddb3bf8
WolfAT14L <32153148+WolfAT14L@users.noreply.github.com>
2017-09-27 15:54:06 +0700

    Update Bai04_giai_phuong_trinh_trung_phuong.cpp

--- level_02/Bai04_giai_phuong_trinh_trung_phuong.cpp
+++ level_02/Bai04_giai_phuong_trinh_trung_phuong.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	float a,b,c,d,x1,x2;
-	printf("nhap cac so a, b, c cua phuong trinh trung phuong ");
+	printf("nhap he so a, b, c cua phuong trinh trung phuong ");
 	scanf("%f%f%f",&a,&b,&c);
 	d=pow(b,2)-4*a*c;
 	x1=(-pow(b,2)-sqrt(d))/(2*a);
@@ -11,7 +11,7 @@ int main()
 	if (d>0 && x1>0 && x2> 0) printf("phuong trinh co 4 nghiem x1= %.1f \t x2= %.1f \t x3= -%.1f \t x4= -%.1f",x1,x2,x1,x2);
 	if (d>0)
 		if(x1>0||x2>0)
-			if (x1>x2)	printf("phuong trinh co 2 nghiem x1= %.1f \t x2= -%.1f ",x1,x1);
+			if (x1>x2) printf("phuong trinh co 2 nghiem x1= %.1f \t x2= -%.1f ",x1,x1);
 				else printf("phuong trinh co 2 nghiem x1= %.1f \t x2= -%.1f ",x2,x2);
 		else printf("phuong trinh vo ngiem");
 	if (d<0) printf("phuong trinh vo ngiem"); 
