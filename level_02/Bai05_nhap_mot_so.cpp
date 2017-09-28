b61487126f2e83b7d33142d97548c1b39b406c96
WolfAT14L <32153148+WolfAT14L@users.noreply.github.com>
2017-09-28 12:54:26 +0700

    Update Bai05_nhap_mot_so.cpp

--- level_02/Bai05_nhap_mot_so.cpp
+++ level_02/Bai05_nhap_mot_so.cpp
@@ -9,7 +9,7 @@ int main()
 	if (a==1)
 		{	
 			float a,b,c,C,S,p;
-			printf("nhap do dai 3 canh cua tam giac ");
+			printf("Nhap do dai 3 canh cua tam giac ");
 			scanf("%f%f%f",&a,&b,&c);
 			C=a+b+c;
 			p=C/2;
@@ -17,34 +17,35 @@ int main()
 			printf("Chu vi %0.2f",C);
 			printf("\nDien tich %0.2f",S);
 		}
-	if (a==2) 
-		{
+		else if (a==2) 
+			{
 			float a,C,S;
-			printf("nhap do dai canh hinh vuong ");
+			printf("Nhap do dai canh hinh vuong ");
 			scanf("%f",&a);
 			S=a*a;
 			C=4*a;
 			printf("Chu vi %0.2f",C);
 			printf("\nDien tich %0.2f",S);
-		}
-	if (a==3)
-		{
-			float a,b,C,S;
-			printf("nhap do dai 2 canh cua hinh chu nhat ");
-			scanf("%f%f",&a,&b);
-			S=a*b;
-			C=2*a+2*b;
-			printf("Chu vi %0.2f",C);
-			printf("\nDien tich %0.2f",S);	
-		}
-	if (a==4)
-		{
-			float a,C,S;
-			printf("nhap do dai ban kinh r ");
-			scanf("%f",&a);
-			S=a*a*PI;
-			C=2*a*PI;
-			printf("Chu vi %0.2f",C);
-			printf("\nDien tich %0.2f",S);
-		}
+			}
+			else if (a==3)
+				{
+				float a,b,C,S;
+				printf("Nhap do dai 2 canh cua hinh chu nhat ");
+				scanf("%f%f",&a,&b);
+				S=a*b;
+				C=2*a+2*b;
+				printf("Chu vi %0.2f",C);
+				printf("\nDien tich %0.2f",S);	
+				}
+				else if (a==4)
+					{
+					float a,C,S;
+					printf("Nhap do dai ban kinh r ");
+					scanf("%f",&a);
+					S=a*a*PI;
+					C=2*a*PI;
+					printf("Chu vi %0.2f",C);
+					printf("\nDien tich %0.2f",S);
+					}
+	else printf ("Ban da nhap sai ma so \nXin moi ban mo lai chuong trinh");
 }
