0c1fe9d5911b99969ed8b72fd06f32dd4df0091c
WolfAT14L <32153148+WolfAT14L@users.noreply.github.com>
2017-10-20 19:39:55 +0700

    Add files via upload

---
+++ level_04/Gia_tri_nho_nhat.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+	int i, min;
+	int a[10];
+	for( i=1;i<10;i++) {
+			printf("Phan tu thu %d= ",i);
+			scanf("%d", &a[i]);
+		} 
+	min=a[1];
+	for( i=2;i<10;i++) {
+		if (min>a[i]) min=a[i]; 
+	}
+	printf("Gia tri nho nhat cua mang la %d",min);
+}
